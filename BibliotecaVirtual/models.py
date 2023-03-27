5a5d0ca2669eef2ca95dfc9cab04596ff6673ddd
Pablo Grigolato <pablogrigolato@gmail.com>
2023-03-27 00:00:19 -0300

    27/3/2023

--- BibliotecaVirtual/models.py
+++ BibliotecaVirtual/models.py
@@ -7,11 +7,17 @@ class Libro(models.Model):
     autor = models.CharField(max_length=40)
     codigo = models.IntegerField()
 
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Autor: {self.autor} - Codigo: {self.codigo}"
+
 class Alumno(models.Model):
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
     dni = models.IntegerField()
     email = models.EmailField()
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - DNI: {self.dni} - Email: {self.email}"
 
 class Docente(models.Model):
     nombre = models.CharField(max_length=40)
@@ -19,3 +25,6 @@ class Docente(models.Model):
     dni = models.IntegerField()
     email = models.EmailField()
     asignatura = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - DNI: {self.dni} - Email: {self.email} - Asignatura: {self.asignatura}"
--- BibliotecaVirtual/views.py
+++ BibliotecaVirtual/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from BibliotecaVirtual.models import Libro, Alumno, Docente
 from BibliotecaVirtual.forms import LibroFormulario, AlumnoFormulario, DocenteFormulario
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -63,13 +67,13 @@ def docente(request):
 
         if miFormulario.is_valid:
             informacion = miFormulario.cleaned_data
-            alumno = Docente(nombre=request.POST['nombre'],
+            docente = Docente(nombre=request.POST['nombre'],
                       apellido=request.POST['apellido'],
                       dni=request.POST['dni'],
                       email=request.POST['email'],
                       asignatura=request.POST['asignatura']
                       )
-            alumno.save()
+            docente.save()
             return render(request, "BibliotecaVirtual/inicio.html")
         
     else:
@@ -89,3 +93,174 @@ def buscar(request):
         respuesta = "No enviaste datos."
         
     return render(request, "BibliotecaVirtual/inicio.html", {"respuesta":respuesta})
+
+def leerDocentes(request):
+    docentes = Docente.objects.all()
+    contexto = {"docentes": docentes}
+    return render(request, "BibliotecaVirtual/leerDocentes.html", contexto)
+
+def leerAlumnos(request):
+    alumnos = Alumno.objects.all()
+    contexto = {"alumnos": alumnos}
+    return render(request, "BibliotecaVirtual/leerAlumnos.html", contexto)
+
+def leerLibros(request):
+    libros = Libro.objects.all()
+    contexto = {"libros": libros}
+    return render(request, "BibliotecaVirtual/leerLibros.html", contexto)
+
+def eliminarDocente(request, docente_nombre):
+    docente = Docente.objects.get(nombre=docente_nombre)
+    docente.delete()
+    docentes = Docente.objects.all()
+    contexto = {"docentes": docentes}
+    return render(request, "BibliotecaVirtual/leerDocentes.html", contexto)
+
+def eliminarAlumno(request, alumno_nombre):
+    alumno = Alumno.objects.get(nombre=alumno_nombre)
+    alumno.delete()
+    alumnos = Alumno.objects.all()
+    contexto = {"alumnos": alumnos}
+    return render(request, "BibliotecaVirtual/leerAlumnos.html", contexto)
+
+def eliminarLibro(request, libro_nombre):
+    libro = Libro.objects.get(nombre=libro_nombre)
+    libro.delete()
+    libros = Libro.objects.all()
+    contexto = {"libros": libros}
+    return render(request, "BibliotecaVirtual/leerLibros.html", contexto)
+
+def editarDocente(request, docente_nombre):
+    docente = Docente.objects.get(nombre=docente_nombre)
+    if request.method == 'POST':
+        miFormulario = DocenteFormulario(request.POST)
+        print(miFormulario)
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+            docente.nombre = informacion['nombre']
+            docente.apellido = informacion['apellido']
+            docente.dni = informacion['dni']
+            docente.email = informacion['email']
+            docente.asignatura = informacion['asignatura']
+            docente.save()
+            return render(request, "BibliotecaVirtual/inicio.html")
+    else:
+        miFormulario = DocenteFormulario(initial={'nombre':docente.nombre,
+                                                  'apellido':docente.apellido,
+                                                  'dni':docente.dni,
+                                                  'email':docente.email,
+                                                  'asignatura':docente.asignatura})
+        return render(request, "BibliotecaVirtual/editarDocente.html", {"miFormulario":miFormulario, "docente_nombre":docente_nombre})
+
+def editarAlumno(request, alumno_nombre):
+    alumno = Alumno.objects.get(nombre=alumno_nombre)
+    if request.method == 'POST':
+        miFormulario = AlumnoFormulario(request.POST)
+        print(miFormulario)
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+            alumno.nombre = informacion['nombre']
+            alumno.apellido = informacion['apellido']
+            alumno.dni = informacion['dni']
+            alumno.email = informacion['email']
+            alumno.save()
+            return render(request, "BibliotecaVirtual/inicio.html")
+    else:
+        miFormulario = AlumnoFormulario(initial={'nombre':alumno.nombre,
+                                                  'apellido':alumno.apellido,
+                                                  'dni':alumno.dni,
+                                                  'email':alumno.email})
+        return render(request, "BibliotecaVirtual/editarAlumno.html", {"miFormulario":miFormulario, "alumno_nombre":alumno_nombre})
+
+def editarLibro(request, libro_nombre):
+    libro = Libro.objects.get(nombre=libro_nombre)
+    if request.method == 'POST':
+        miFormulario = LibroFormulario(request.POST)
+        print(miFormulario)
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+            libro.nombre = informacion['nombre']
+            libro.autor = informacion['autor']
+            libro.codigo = informacion['codigo']
+            libro.save()
+            return render(request, "BibliotecaVirtual/inicio.html")
+    else:
+        miFormulario = LibroFormulario(initial={'nombre':libro.nombre,
+                                                  'autor':libro.autor,
+                                                  'codigo':libro.codigo})
+        return render(request, "BibliotecaVirtual/editarLibro.html", {"miFormulario":miFormulario, "libro_nombre":libro_nombre})
+
+class LibroList(ListView):
+    model = Libro
+    template_name = "BibliotecaVirtual/libros_list.html"
+
+#class AlumnoList(ListView):
+#    model = Alumno
+#    template_name = "BibliotecaVirtual/alumnos_list.html"
+#
+#class DocenteList(ListView):
+#    model = Docente
+#    template_name = "BibliotecaVirtual/docentes_list.html"
+
+class LibroDetalle(DetailView):
+    model = Libro
+    template_name = "BibliotecaVirtual/libro_detalle.html"
+
+#class AlumnoDetalle(DetailView):
+#    model = Alumno
+#    template_name = "BibliotecaVirtual/alumno_detalle.html"
+#
+#class DocenteDetalle(DetailView):
+#    model = Docente
+#    template_name = "BibliotecaVirtual/docente_detalle.html"
+
+class LibroCreacion(CreateView):
+    model = Libro
+    template_name = "BibliotecaVirtual/libro_form.html"
+    success_url = "/BibliotecaVirtual/libro/list"
+    fields = ['nombre', 'autor', 'codigo']
+
+#class AlumnoCreacion(CreateView):
+#    model = Alumno
+#    template_name = "BibliotecaVirtual/alumnos_form.html"
+#    success_url = reverse_lazy("BibliotecaVirtual:List")
+#    fields = ['nombre', 'apellido', 'dni']
+#
+#class DocenteCreacion(CreateView):
+#    model = Docente
+#    template_name = "BibliotecaVirtual/docentes_form.html"
+#    success_url = reverse_lazy("BibliotecaVirtual:List")
+#    fields = ['nombre', 'apellido', 'dni']
+
+class LibroUpdate(UpdateView):
+    model = Libro
+    success_url = "/BibliotecaVirtual/libro/list"
+    template_name = "BibliotecaVirtual/libro_form.html"
+    fields = ['nombre', 'autor', 'codigo']
+
+#class AlumnoUpdate(UpdateView):
+#    model = Alumno
+#    success_url = "/BibliotecaVirtual/alumno/list"
+#    template_name = "alumnos_form.html"
+#    fields = ['nombre', 'apellido', 'dni']
+#
+#class DocenteUpdate(UpdateView):
+#    model = Docente
+#    success_url = "/BibliotecaVirtual/docente/list"
+#    template_name = "docentes_form.html"
+#    fields = ['nombre', 'apellido', 'dni']
+
+class LibroDelete(DeleteView):
+    model = Libro
+    template_name = "BibliotecaVirtual/libro_confirm_delete.html"
+    success_url = "/BibliotecaVirtual/libro/list"
+
+#class AlumnoDelete(DeleteView):
+#    model = Alumno
+#    template_name = "BibliotecaVirtual/alumno_confirm_delete.html"
+#    success_url = "/BibliotecaVirtual/alumno/list"
+#
+#class DocenteDelete(DeleteView):
+#    model = Docente
+#    template_name = "BibliotecaVirtual/docente_confirm_delete.html"
+#    success_url = "/BibliotecaVirtual/docente/list"
